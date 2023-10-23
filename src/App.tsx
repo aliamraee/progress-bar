7b0fce95cf06c8193a934d6aee8bd6b4cd6e8d9c
Ali  Amraei <Ali.Omrai@rashasoft.ir>
2023-10-23 20:32:09 +0330

    config tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import ProgressBar from './components/ProgressBar'
 
 function App() {
 
   return (
-    <div>
-      <ProgressBar progress={80} title='Progress' description='this is test description' />
+    <div className='w-full h-screen flex justify-center items-center'>
+      <div className="w-96 flex ">
+        <ProgressBar type='info' progress={80} title='Progress' description='this is test description' />
+
+
+      </div>
     </div>
   )
 }
--- src/components/ProgressBar/progressBar-component.tsx
+++ src/components/ProgressBar/progressBar-component.tsx
@@ -1,20 +1,32 @@
 interface PropsType {
     title?: string;
     description?: string;
-    progress: number
+    progress: number,
+    type?: string
 }
 
-const ProgressBar = ({ title, description, progress }: PropsType) => {
+const ProgressBar = ({ title, description, progress, type }: PropsType) => {
+
+    const getTypeColor = () => {
+        switch (type) {
+            case "info": return { backgroundStyle: "bg-blue-500", textStyle: "text-neutral-700 text-sm" }
+            case "success": return { backgroundStyle: "bg-green-500", textStyle: "text-neutral-600" }
+            case "error": return { backgroundStyle: "bg-red-500", textStyle: "text-red-500" }
+            default: return { backgroundStyle: 'bg-blue-500 ', textStyle: "text-neutral-600" };
+        }
+    }
+
+    const customStyle = getTypeColor()
     return (
-        <div className="progress-bar-root">
-            <span>{title}</span>
-            <div className='progress-bar-container'>
-                <div className="progress-bar">{progress}%</div>
+        <div className="progress-bar-root w-full ">
+            <span className="text-sm font-bold pb-1 block">{title}</span>
+            <div className={`progress-bar-container w-full h-3 bg-gray-200 rounded`}>
+                <div className={`progress-bar text-sm h-full ${customStyle.backgroundStyle} rounded`} style={{ width: `${progress}%` }}></div>
             </div>
-            <span>
+            <span className={`${customStyle.textStyle} pt-1 block`}>
                 {description}
             </span>
-        </div>
+        </div >
     )
 }
 
